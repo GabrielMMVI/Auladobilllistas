2071c3e075991691bc196b8356757b28540d3b62
GabrielMMVI <gabrielpessi5505@gmail.com>
2024-10-21 20:19:22 -0300

    VAi porra

--- main.c
+++ main.c
@@ -1,6 +1,34 @@
 #include <stdio.h>
+#define MAX 5
+void inserir(int lista[], int *tamanho, int valor){
+    if (*tamanho < MAX){
+        lista [*tamanho] = valor;
+        (*tamanho)++;
+    } else {
+        printf("Lista cheia!");
+
+    }
+}
+
+void exibir(int lista[], int tamanho){
+    printf("Elementos da lista: ");
+    for (int i = 0; i < tamanho; i++)
+    {
+        printf("%d", lista[i]);
+    }
+    printf("\n");
+}
 
 int main() {
-    printf("Hello, World!\n");
+    int lista [MAX];
+    int tamanho = 0;
+    inserir(lista, &tamanho, 3);
+    inserir(lista, &tamanho, 22);
+    inserir(lista, &tamanho, 127);
+    inserir(lista, &tamanho, 24);
+    inserir(lista, &tamanho, 521);
+    inserir(lista, &tamanho, 0);
+
+    exibir(lista, tamanho);
     return 0;
 }
